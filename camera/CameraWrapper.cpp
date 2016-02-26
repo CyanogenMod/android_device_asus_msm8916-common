0cfe2c0d5d86679f51fd6c633f132610eb1b162a
LuK1337 <priv.luk@gmail.com>
2016-02-26 18:47:55 +0100

    msm8916-common: camera: Save cur-focus-scale and cur-focus-diopter
    * Those params are used in Snap camera. Without
    setting them it simply says the current value
    is "null".
    
    Change-Id: Ia45123dcf99a521d6b541c30c058ff6e8bc56424

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -118,6 +118,16 @@ static char *camera_fixup_getparams(int id, const char *settings)
 
     params.set("longshot-supported", "false");
 
+    const char *manualFocusPosition = params.get("manual-focus-position");
+    const char *manualFocusPositionType = params.get("manual-focus-pos-type");
+    if (manualFocusPosition != NULL) {
+        if (!strcmp(manualFocusPositionType, "2")) {
+            params.set("cur-focus-scale", manualFocusPosition);
+        } else if (!strcmp(manualFocusPositionType, "3")) {
+            params.set("cur-focus-diopter", manualFocusPosition);
+        }
+    }
+
 #if !LOG_NDEBUG
     ALOGV("%s: fixed parameters:", __FUNCTION__);
     params.dump();
