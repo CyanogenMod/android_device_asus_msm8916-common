9a4799e0a0d295032ea4929062e387e0851159ff
Christopher R. Palmer <crpalmer@gmail.com>
2016-02-27 14:11:13 -0500

    msm8916-common: audio: Add amplifier hal
    Change-Id: I36b31d42e80f7cbb0590f318a0dae02aac471d36

---
+++ amplifier/audio_amplifier.c
@@ -0,0 +1,189 @@
+/*
+ * Copyright (C) 2016, The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "audio_amplifier"
+//#define LOG_NDEBUG 0
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <cutils/log.h>
+#include <cutils/str_parms.h>
+
+#include <hardware/audio_amplifier.h>
+
+#include <msm8916/platform.h>
+
+#include <system/audio.h>
+#include <tinyalsa/asoundlib.h>
+
+typedef struct amp_device {
+    amplifier_device_t amp_dev;
+    audio_mode_t current_mode;
+} amp_device_t;
+
+static amp_device_t *amp_dev = NULL;
+
+extern int tfa9887_dualcalibration_startup(void);
+extern int tfa9887_dualspeakeron(uint32_t);
+extern int tfa9887_dualspeakeroff();
+extern int tfa98xx_set_volume(float, float);
+extern int tfa98xx_set_vstep(uint32_t, uint32_t);
+
+#define AMP_MIXER_CTL "QUAT_MI2S_RX Audio Mixer MultiMedia1"
+
+static int set_clocks_enabled(bool enable)
+{
+    enum mixer_ctl_type type;
+    struct mixer_ctl *ctl;
+    struct mixer *mixer = mixer_open(0);
+
+    if (mixer == NULL) {
+        ALOGE("Error opening mixer 0");
+        return -1;
+    }
+
+    ctl = mixer_get_ctl_by_name(mixer, AMP_MIXER_CTL);
+    if (ctl == NULL) {
+        mixer_close(mixer);
+        ALOGE("%s: Could not find %s\n", __func__, AMP_MIXER_CTL);
+        return -ENODEV;
+    }
+
+    type = mixer_ctl_get_type(ctl);
+    if (type != MIXER_CTL_TYPE_ENUM) {
+        ALOGE("%s: %s is not supported\n", __func__, AMP_MIXER_CTL);
+        mixer_close(mixer);
+        return -ENOTTY;
+    }
+
+    mixer_ctl_set_value(ctl, 0, enable);
+    mixer_close(mixer);
+    return 0;
+}
+
+static int amp_set_mode(amp_device_t *device, audio_mode_t mode)
+{
+    int ret = 0;
+    amp_device_t *dev = (amp_device_t *) device;
+
+    dev->current_mode = mode;
+    return ret;
+}
+
+static int amp_set_output_devices(amp_device_t *device, uint32_t devices)
+{
+    amp_device_t *dev = (amp_device_t *) device;
+
+    return 0;
+}
+
+static int amp_enable_output_devices(amp_device_t *device,
+        uint32_t devices, bool enable)
+{
+    amp_device_t *dev = (amp_device_t *) device;
+
+    switch (devices) {
+        case SND_DEVICE_OUT_SPEAKER:
+        case SND_DEVICE_OUT_SPEAKER_REVERSE:
+        case SND_DEVICE_OUT_VOICE_SPEAKER:
+        case SND_DEVICE_OUT_SPEAKER_PROTECTED:
+        case SND_DEVICE_OUT_SPEAKER_AND_HEADPHONES:
+           if (enable) {
+               set_clocks_enabled(true);
+               tfa98xx_set_vstep(1, 1);
+               tfa9887_dualspeakeron(enable);
+           } else {
+               tfa9887_dualspeakeroff(false);
+               set_clocks_enabled(false);
+           }
+    }
+    return 0;
+}
+
+static int amp_dev_close(hw_device_t *device)
+{
+    amp_device_t *dev = (amp_device_t *) device;
+
+    free(dev);
+
+    return 0;
+}
+
+static int amp_module_open(const hw_module_t *module, const char *name,
+        hw_device_t **device)
+{
+    char PRJ_ID[256];
+
+    FILE *fp = fopen("/proc/apid" , "r");
+    fgets(PRJ_ID, sizeof(PRJ_ID), fp);
+    pclose(fp);
+
+    if (!strstr(PRJ_ID, "0")) {
+        return 1;
+
+    if (amp_dev) {
+        ALOGE("%s:%d: Unable to open second instance of the amplifier\n",
+                __func__, __LINE__);
+        return -EBUSY;
+    }
+
+    amp_dev = calloc(1, sizeof(amp_device_t));
+    if (!amp_dev) {
+        ALOGE("%s:%d: Unable to allocate memory for amplifier device\n",
+                __func__, __LINE__);
+        return -ENOMEM;
+    }
+
+    amp_dev->amp_dev.common.tag = HARDWARE_DEVICE_TAG;
+    amp_dev->amp_dev.common.module = (hw_module_t *) module;
+    amp_dev->amp_dev.common.version = HARDWARE_DEVICE_API_VERSION(1, 0);
+    amp_dev->amp_dev.common.close = amp_dev_close;
+
+    amp_dev->amp_dev.set_input_devices = NULL;
+    amp_dev->amp_dev.set_output_devices = amp_set_output_devices;
+    amp_dev->amp_dev.enable_input_devices = NULL;
+    amp_dev->amp_dev.enable_output_devices = amp_enable_output_devices;
+    amp_dev->amp_dev.set_mode = amp_set_mode;
+    amp_dev->amp_dev.output_stream_start = NULL;
+    amp_dev->amp_dev.input_stream_start = NULL;
+    amp_dev->amp_dev.output_stream_standby = NULL;
+    amp_dev->amp_dev.input_stream_standby = NULL;
+
+    amp_dev->current_mode = AUDIO_MODE_NORMAL;
+
+    *device = (hw_device_t *) amp_dev;
+
+    set_clocks_enabled(true);
+    tfa9887_dualcalibration_startup();
+    set_clocks_enabled(false);
+
+    return 0;
+
+static struct hw_module_methods_t hal_module_methods = {
+    .open = amp_module_open,
+};
+
+amplifier_module_t HAL_MODULE_INFO_SYM = {
+    .common = {
+        .tag = HARDWARE_MODULE_TAG,
+        .module_api_version = AMPLIFIER_MODULE_API_VERSION_0_1,
+        .hal_api_version = HARDWARE_HAL_API_VERSION,
+        .id = AMPLIFIER_HARDWARE_MODULE_ID,
+        .name = "MSM8916 audio amplifier HAL",
+        .author = "The CyanogenMod Open Source Project",
+        .methods = &hal_module_methods,
+    },
+};
