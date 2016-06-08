7d09e18bf2fe23dcde3d35f43d4fa73ca8265f0e
luca020400 <luca.stefani.ge1@gmail.com>
2016-06-08 23:48:53 +0200

    msm8916-common: Add unifed Z00L and Z00T init
    Change-Id: Iba6a23977d885e2699415b50bef93cef8241abeb

---
+++ init/init_msm8916.cpp
@@ -0,0 +1,108 @@
+/*
+   Copyright (c) 2013, The Linux Foundation. All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "vendor_init.h"
+#include "property_service.h"
+#include "log.h"
+#include "util.h"
+
+#define ISMATCH(a,b)    (!strncmp(a,b,PROP_VALUE_MAX))
+
+char const *device;
+char const *family;
+
+void check_device()
+{
+    char PRJ_ID[PROP_VALUE_MAX];
+    char PRJ_SKU[PROP_VALUE_MAX];
+    char PRJ_HD[PROP_VALUE_MAX];
+    FILE *fp;
+
+    fp = fopen("/proc/apid", "r");
+    fgets(PRJ_ID, sizeof(PRJ_ID), fp);
+    pclose(fp);
+
+    fp = fopen("/proc/aprf", "r");
+    fgets(PRJ_SKU, sizeof(PRJ_SKU), fp);
+    pclose(fp);
+
+    fp = fopen("/proc/aphd", "r");
+    fgets(PRJ_HD, sizeof(PRJ_HD), fp);
+    pclose(fp);
+
+    if (ISMATCH(PRJ_HD, "1\n")) {
+        family = "Z00L";
+        if (ISMATCH(PRJ_ID, "0\n")) {
+            if (ISMATCH(PRJ_SKU, "3\n")) {
+                device = "Z00W";
+            } else {
+                device = "Z00L";
+            }
+        } else if (ISMATCH(PRJ_ID, "1\n")) {
+            device = "Z00M";
+        }
+    } else {
+        family = "Z00T";
+        if (ISMATCH(PRJ_ID, "0\n")) {
+            device = "Z00T";
+        } else if (ISMATCH(PRJ_ID, "1\n")) {
+            device = "Z011";
+        } else if (ISMATCH(PRJ_ID, "2\n")) {
+            device = "Z00C";
+        } else if (ISMATCH(PRJ_ID, "3\n")) {
+            device = "Z00U";
+        }
+    }
+}
+
+void vendor_load_properties()
+{
+    char platform[PROP_VALUE_MAX];
+    int rc;
+
+    rc = property_get("ro.board.platform", platform);
+    if (!rc || !ISMATCH(platform, ANDROID_TARGET))
+        return;
+
+    check_device();
+
+    property_set("ro.build.product", family);
+    property_set("ro.build.description", ("%s-user 6.0.1 MMB29P WW_user_21.40.1220.1615_20160517 release-keys", family));
+
+    property_set("ro.product.model", ("ASUS_%sD", device));
+    property_set("ro.product.device", ("ASUS_%s", device));
+    property_set("ro.product.carrier", ("US-ASUS_%s-WW_%s", device, device));
+    property_set("ro.build.fingerprint", ("asus/WW_%s/ASUS_%s:6.0.1/MMB29P/WW_user_21.40.1220.1615_20160517:user/release-keys", device));
+
+    INFO("Setting build properties for %s device of %s family\n", device, family);
+}
