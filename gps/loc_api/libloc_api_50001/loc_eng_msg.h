94cf89a2907801cfb7967587d41047a291e945a4
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-02 10:54:33 +0200

    msm8916-common: gps: HAX
    Change-Id: Ie79c5471423ce0d39e34f1dd0a3e71d9edb016b0

--- gps/loc_api/libloc_api_50001/loc_eng_msg.h
+++ gps/loc_api/libloc_api_50001/loc_eng_msg.h
@@ -105,11 +105,11 @@ struct LocEngReportPosition : public LocMsg {
 
 struct LocEngReportSv : public LocMsg {
     LocAdapterBase* mAdapter;
-    const QcomGnssSvStatus mSvStatus;
+    const PnssSvStatus mSvStatus;
     const GpsLocationExtended mLocationExtended;
     const void* mSvExt;
     LocEngReportSv(LocAdapterBase* adapter,
-                   QcomGnssSvStatus &sv,
+                   PnssSvStatus &sv,
                    GpsLocationExtended &locExtended,
                    void* svExtended);
     virtual void proc() const;
